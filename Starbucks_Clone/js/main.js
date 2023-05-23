3e9b51b1f80166baae91687625afbb6c49e62a0d
rktdnjs <rktdnjs@naver.com>
2023-05-23 21:45:23 +0900

    feat : Add Vertical Slide & Horizontal Slide
    Part 1. 16 ~ 20

--- Starbucks_Clone/js/main.js
+++ Starbucks_Clone/js/main.js
@@ -73,4 +73,31 @@ fadeEls.forEach(function (fadeEl, index) {
         delay : (index + 1) * 0.7,
         opacity : 1
     })
+}); 
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction : 'vertical',
+    autoplay : true,
+    loop : true,
 });
+
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal' 기본값!
+    slidesPerView : 3, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween : 10, // 슬라이드 사이의 여백이 설정됨
+    centeredSlides : true, // 1번 슬라이드가 가운데에 보이기 설정
+    loop : true, // 슬라이드 반복
+    autoplay : {
+        delay : 5000// 기본값 : 3000(3초)
+    },
+    pagination : {
+        el : '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable : true, // 사용자의 페이지 번호요소 제어 가능 여부
+    },
+    navigation : { // 전 슬라이드와 다음 슬라이드를 볼 수 있는 기능
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
